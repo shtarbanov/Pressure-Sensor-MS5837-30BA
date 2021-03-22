265756fc7f57a5804699382e72220fc919237e15
shtarbanov <alims@media.mit.edu>
2021-03-21 20:28:03 -0400

    Add files via upload

--- MS5837.h
+++ MS5837.h
@@ -4,20 +4,30 @@
 #include "Arduino.h"
 #include <Wire.h> //There are multiple ports in this: Wire, Wire
 
+enum Unit : uint8_t{
+	PSI, ATM, MBAR, KPA, PA
+};
+
 class MS5837 {
 public:
-	float pressure_mbar; //Pressure returned in mbar.
-	float pressure_psi;
-	float pressure_pa; //pascal
-	float pressure_kpa; //kiloPascal
-	float temp_c; // Temperature returned in deg C
+	float pressure_mbar=-1; //Pressure returned in mbar.
+	float pressure_psi=-1;
+	float pressure_pa=-1; //pascal
+	float pressure_kpa=-1; //kiloPascal
+	float pressure_atm=-1;
+	float temp_c=-1; // Temperature returned in deg C
 
 	MS5837(); //constructor
-
-	bool initPressureSensor();
-	void read();	//read from I2C takes up to 40 ms, so use sparingly if possible.
+	bool initSensor();
+	void requestPressureAndTemp();	//read from I2C takes up to 40 ms, so use sparingly if possible.
+	void setPressureUnit(Unit pUnit); //default is PSI.
+	float getPressure();
+	float getPressure(Unit pUnit);
+	uint8_t readError();
 private:
-	TwoWire * _i2cPort;//This stores the requested i2c port
+	TwoWire * _i2c;//This stores the requested i2c port
+	bool _i2cInitialized=false;
+	Unit _pressureUnit=PSI;
 	uint16_t C[8];
 	uint32_t D1_pres, D2_temp;
 	int32_t TEMP;
@@ -25,5 +35,7 @@ private:
 
 	void calculate(); //Performs calculations per the sensor data sheet for conversion and second order compensation.
 	uint8_t crc4(uint16_t n_prom[]);
+
+	uint8_t _errorCode = 0; //this will change value if an error occurs.
 };
 #endif
